ffd9fcee21e3899ea3cc37b2b957178cacfbd6f8
AceOfHeart5 <evanconwaymusic@gmail.com>
2023-09-29 17:31:06 -0400

    fix channel id must be number bug

--- bot.py
+++ bot.py
@@ -4,15 +4,21 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
-CHANNEL_ID = os.getenv('CHANNEL_ID')
+CHANNEL_ID = int(os.getenv('CHANNEL_ID'))
 
 from discord.ext import commands
 import discord
 
-bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
-    print("Hello! Bot is ready.")
+    print('Hello! Bot is ready.')
+    channel = bot.get_channel(CHANNEL_ID)
+    if channel == None:
+        print('channel could not be found')
+        print('tried to find channel id: ' + CHANNEL_ID)
+    else:
+        await channel.send('I am the bot and I am here.')
 
 bot.run(BOT_TOKEN)
